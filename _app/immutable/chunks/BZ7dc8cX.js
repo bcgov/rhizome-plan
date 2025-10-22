757b80f212dfd8e7dcb5cd4c6a7c9ddad6ebe42c
Jeremy Vernon <jeremyvernon.com@gmail.com>
2025-10-22 14:43:33 -0700

    Static Site Build - 6
    Added new images, corrected some minor layout issues.

---
+++ _app/immutable/chunks/BZ7dc8cX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_3mg0fx?.base??"/rhizome-plan",a=globalThis.__sveltekit_3mg0fx?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/Bm4SuQch.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_166yelu?.base??"/rhizome-plan",e=globalThis.__sveltekit_166yelu?.assets??s??"";export{e as a,s as b};
